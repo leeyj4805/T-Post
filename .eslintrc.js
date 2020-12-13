9a639218ebd7450aad1ecae1c2f762c4ea04e3a7
leebingo <leeyj4805@gmail.com>
2020-12-13 19:26:08 +0900

    :white_check_mark: eslint 설정 추가 및 변경 - eslint의 룰 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,5 +30,7 @@ export default {
          // Place to specify ESLint rules.
          // Can be used to overwrite rules specified from the extended configs
          // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+         "@typescript-eslint/explicit-module-boundary-types": "off",
+         "@typescript-eslint/no-explicit-any": "off"
     }
 };
