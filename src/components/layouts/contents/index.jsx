130ef9a0b4ec2ab6c9420d2654f6d0abf3e52bea
leebingo <leeyj4805@gmail.com>
2021-03-23 02:55:05 +0900

    :memo: Mypage 컴포넌트 추가 수정 (#35)

--- src/components/layouts/contents/index.jsx
+++ src/components/layouts/contents/index.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { AddIcon } from "@chakra-ui/icons";
 import "./index.scss";
 
 function contents() {
   return (
     <div className="user">
       안녕하세요 <span className="userName">침착맨</span>님!
+      <div className="userBox">
+        <h2 className="userheader">
+          받은 편지지
+          <a href="//#endregion">
+            <AddIcon w={4} h={4} />
+          </a>
+        </h2>
+      </div>
     </div>
   );
 }
--- src/pages/Photo/Photo.jsx
+++ src/pages/Photo/Photo.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import "./Photo.scss";
+import Hello from "./MainPhoto";
 
-function Photo() {
+
+const Photo = () => {
   return (
     <div className="Photo">
-        <h1 className="PhotoHeader">침착맨
-          <span className="PhotoHeaderSmall">(zilione)</span>
-        </h1>
-        <span className="Subscriber">구독자<p className="number ml">76.9만명</p></span>
+      <a href="//#endregion">
+        <Hello isSpecial={true}></Hello>
+      </a>
+      <h1 className="PhotoHeader">
+        침착맨
+        <span className="PhotoHeaderSmall">(zilione)</span>
+      </h1>
+      <span className="Subscriber">
+        구독자<p className="number ml">76.9만명</p>
+      </span>
     </div>
   );
-}
+};
 
 export default Photo;
 
