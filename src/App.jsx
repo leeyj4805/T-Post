19225790a561cb5e2bd84f6c92210f16a3f23a29
clianor <ejrgus94160@gmail.com>
2021-03-28 12:24:02 +0900

    로그인 기본 로직 구현
    Signed-off-by: clianor <ejrgus94160@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, { lazy } from "react";
 import { Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Spin } from "antd";
+import { CircularProgress } from "@chakra-ui/react";
 
 const LoginPage = lazy(() => import("./pages/auth/login"));
 
 export default function App() {
   return (
     <Router>
-      <Suspense fallback={<Spin size="large" />}>
+      <Suspense fallback={<CircularProgress size="100px" />}>
         <Switch>
           <Route path="/login" component={LoginPage} />
           <Route path="*" component={() => <div>Not Found</div>} />
--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, ButtonGroup } from "@chakra-ui/react";
 import logo from "../../assets/images/t-logo.png";
 import "./login.scss";
+import { useHistory, useLocation } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
-const response_type = "code";
-const client_id = process.env.REACT_APP_CLIENT_ID;
-const scope = "user:read:email user_read moderation:read";
-const redirect_uri = window.location.origin;
+const redirectUrl = "http://127.0.0.1:3000/login";
 
 function Login() {
+  const location = useLocation();
+  const history = useHistory();
+  const [searchParam, setSearchParam] = useState("");
+
+  const { fetch, loading, data, error } = useFetch({
+    method: "get",
+    url: `https://dev.api.tpost.lol/login/twitch${searchParam}`,
+  });
+
   const handleLogin = () => {
-    window.location.href = `https://id.twitch.tv/oauth2/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=${response_type}&scope=${scope}`;
+    window.location.href = `https://dev.api.tpost.lol/oauth2/authorization/twitch?redirectUrl=${redirectUrl}`;
   };
+
+  useEffect(() => {
+    const searchParam = location.search;
+    setSearchParam(searchParam);
+  }, []);
+
+  useEffect(() => {
+    if (searchParam) {
+      fetch();
+    }
+  }, [searchParam]);
+
+  useEffect(() => {
+    if (!loading) {
+      if (!error && data.status === 200) {
+        history.push("/");
+      }
+    }
+  }, [loading, data, error]);
 
   return (
     <div className="Login">
